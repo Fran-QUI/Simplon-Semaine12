b63b0563b09fe0138bae87b4e6b5d595aefb6817
François QUIVRON <fquivron@gmail.com>
2017-06-02 18:20:58 +0200

    Complete l'exercice 2, les conditions de victoire ne marchent pas pour l'instant

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -29,6 +29,10 @@ var $messages = document.getElementById("messages");
 
 
 $zoneDeJeu.addEventListener('click', function(e) {
+
+
+/*Teste le tour du joueur et ajoute une class CSS*/
+	
 	if (e.target.classList == "case") {
 		switch(tourJoueur) {
 			case 1 :
@@ -44,15 +48,175 @@ $zoneDeJeu.addEventListener('click', function(e) {
 		$messages.innerHTML="Cette case à déjà été jouée, choisissez en une autre...";
 		setTimeout(function(){$messages.innerHTML=""}, 1500);
 	}
-	victoire();
+
+/*teste les class pour voire si une condition de victoire est présente pour chacun des joueurs*/
+
+	if (
+
+////////////
+//joueur 1//
+////////////
+
+//Les Lignes//
+
+/*		  1 2 3
+		1 X X X
+        2
+		3         */
+
+	$l1c1.classList.contains("joueur1") == $l1c2.classList.contains("joueur1") &&
+	$l1c1.classList.contains("joueur1") == $l1c3.classList.contains("joueur1") ||
+		
+/*		  1 2 3
+		1
+        2 X X X
+		3         */
+
+	$l2c1.classList.contains("joueur1") == $l2c2.classList.contains("joueur1") &&
+	$l2c1.classList.contains("joueur1") == $l2c3.classList.contains("joueur1") ||
+
+/*		  1 2 3
+		1
+		2 
+		3 X X X    */
+
+	$l3c1.classList.contains("joueur1") == $l3c2.classList.contains("joueur1") &&
+	$l3c1.classList.contains("joueur1") == $l3c3.classList.contains("joueur1") ||
+
+//Les Colonnes//
+
+/*		  1 2 3
+		1 X 
+        2 X
+		3 X        */
+
+	$l1c1.classList.contains("joueur1") == $l2c1.classList.contains("joueur1") &&
+	$l1c1.classList.contains("joueur1") == $l3c1.classList.contains("joueur1") ||
+		
+/*		  1 2 3
+		1   X
+        2   X
+		3   X     */
+
+	$l1c2.classList.contains("joueur1") == $l2c2.classList.contains("joueur1") &&
+	$l1c2.classList.contains("joueur1") == $l3c2.classList.contains("joueur1") ||
+
+/*		  1 2 3
+		1     X
+		2     X
+		3     X    */
+
+	$l3c1.classList.contains("joueur1") == $l3c2.classList.contains("joueur1") &&
+	$l3c1.classList.contains("joueur1") == $l3c3.classList.contains("joueur1") ||
+
+//Les Diagonales//
+
+/*		  1 2 3
+		1 X 
+        2   X
+		3     X        */
+
+	$l1c1.classList.contains("joueur1") == $l2c2.classList.contains("joueur1") &&
+	$l1c1.classList.contains("joueur1") == $l3c3.classList.contains("joueur1") ||
+		
+/*		  1 2 3
+		1     X
+        2   X
+		3 X     */
+
+	$l1c3.classList.contains("joueur1") == $l2c2.classList.contains("joueur1") &&
+	$l1c3.classList.contains("joueur1") == $l3c1.classList.contains("joueur1") ||
+		
+////////////
+//joueur 2//
+////////////
+
+//Les Lignes//
+
+/*		  1 2 3
+		1 X X X
+        2
+		3         */
+
+	$l1c1.classList.contains("joueur2") == $l1c2.classList.contains("joueur2") &&
+	$l1c1.classList.contains("joueur2") == $l1c3.classList.contains("joueur2") ||
+		
+/*		  1 2 3
+		1
+        2 X X X
+		3         */
+
+	$l2c1.classList.contains("joueur2") == $l2c2.classList.contains("joueur2") &&
+	$l2c1.classList.contains("joueur2") == $l2c3.classList.contains("joueur2") ||
+
+/*		  1 2 3
+		1
+		2 
+		3 X X X    */
+
+	$l3c1.classList.contains("joueur2") == $l3c2.classList.contains("joueur2") &&
+	$l3c1.classList.contains("joueur2") == $l3c3.classList.contains("joueur2") ||
+
+//Les Colonnes//
+
+/*		  1 2 3
+		1 X 
+        2 X
+		3 X        */
+
+	$l1c1.classList.contains("joueur2") == $l2c1.classList.contains("joueur2") &&
+	$l1c1.classList.contains("joueur2") == $l3c1.classList.contains("joueur2") ||
+		
+/*		  1 2 3
+		1   X
+        2   X
+		3   X     */
+
+	$l1c2.classList.contains("joueur2") == $l2c2.classList.contains("joueur2") &&
+	$l1c2.classList.contains("joueur2") == $l3c2.classList.contains("joueur2") ||
+
+/*		  1 2 3
+		1     X
+		2     X
+		3     X    */
+
+	$l3c1.classList.contains("joueur2") == $l3c2.classList.contains("joueur2") &&
+	$l3c1.classList.contains("joueur2") == $l3c3.classList.contains("joueur2") ||
+
+//Les Diagonales//
+
+/*		  1 2 3
+		1 X 
+        2   X
+		3     X        */
+
+	$l1c1.classList.contains("joueur2") == $l2c2.classList.contains("joueur2") &&
+	$l1c1.classList.contains("joueur2") == $l3c3.classList.contains("joueur2") ||
+		
+/*		  1 2 3
+		1     X
+        2   X
+		3 X     */
+
+	$l1c3.classList.contains("joueur2") == $l2c2.classList.contains("joueur2") &&
+	$l1c3.classList.contains("joueur2") == $l3c1.classList.contains("joueur2") 
+		
+
+
+////////////
+//résultat//
+////////////
+		)
+		{$messages.innerHTML="Victoire";
+		setTimeout(function(){$messages.innerHTML=""}, 1500);
+		};
+	
 	});
 
 
-funtion victoire() {
-	if ($l1c1.classList == $l1c2.classList && $l1c1.classList == $l1c3.classList) {
-		$messages.innerHTML="Victoire";
-	};
-};
+
+	
+
 
 
 
